0d3bdd70a0caa341a9279119fe7c54126f3e22ee
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-01-20 10:19:39 -0700

    generated model

---
+++ db/migrate/20230120171918_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lists do |t|
+      t.string :item
+      t.string :description
+      t.string :priority
+
+      t.timestamps
+    end
+  end
+end
