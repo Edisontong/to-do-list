f547b3f666b813a334987c9641f335cf2828f3c9
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-01-20 10:25:18 -0700

    generated jbuilder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+list = List.create(item: "clean house", description: "deep clean kitchen, do laundry, take out the trash", priority: "high")
+list.save
+
+list = List.create(item: "grocery shopping", description: "eggs, potatoes, butter, apples, popcorn, ice cream", priority: "medium")
+list.save
+
+list = List.create(item: "pack for weekend trip", description: "2 days one night, bring wedding clothes", priority: "low")
+list.save
